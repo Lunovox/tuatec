5b7e0d136ccf75b9df84017fe20a9a61e2c12892
Lunovox <Lunovox@users.noreply.github.com>
2019-01-08 15:06:09 -0200

    Update index.php

--- home2/index.php
+++ home2/index.php
@@ -12,8 +12,7 @@
 		<link rel="stylesheet" href="estilo_mobile.css" media="(max-width:380px)">
 		<link rel="stylesheet" href="estilo_geral.css" media="screen">
 		-->
-		<meta_ name_="viewport" content_="width=380">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 		<link rel="stylesheet" href="estilo_geral.css">
 		
